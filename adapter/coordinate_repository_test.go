40753faa3ce46782adde6e2eda02fb2aba58163a
Ricardo <ricardo@goncalves.biz>
2024-11-30 21:24:14 -0300

    test: adiciona testes para nome de cidade e coordenadas

--- adapter/coordinate_repository_test.go
+++ adapter/coordinate_repository_test.go
@@ -3,17 +3,19 @@ package adapter_test
 import (
 	"context"
 	"github.com/rgoncalvesrr/fullcycle-labs-otel/adapter"
-	"github.com/rgoncalvesrr/fullcycle-labs-otel/configs"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestNewCoordinateRepository(t *testing.T) {
-	r := adapter.NewCoordinateRepository(configs.NewConfig(".."))
+	r := adapter.NewCoordinateRepository()
 	c, e := r.GetByCep(context.Background(), "09130220")
 
 	assert.NotNil(t, r)
 	assert.Nil(t, e)
 	assert.NotNil(t, c)
+	assert.Equal(t, "Santo André", c.City)
+	assert.Equal(t, "-23.6929", c.Latitude)
+	assert.Equal(t, "-46.50423", c.Longitude)
 
 }
